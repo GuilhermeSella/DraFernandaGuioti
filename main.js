edbd168866f487848258e74ca0f5c9dcdf168ae1
Guilherme Sella <guilhermesella1974@gmail.com>
2023-10-28 23:59:16 -0300

    criando layout doutora fernanda

---
+++ main.js
@@ -0,0 +1,28 @@
+const header = document.querySelector('.header')
+
+window.addEventListener("scroll", () => {
+
+    header.classList.toggle('scroll', window.scrollY > 7)
+});
+
+const menuHambuguer = document.querySelector("#menu-hamburguer")
+
+menuHambuguer.addEventListener("click", () => {
+
+    let nav = document.querySelector(".navMobile")
+    menuHambuguer.classList.toggle("hamburguerActive")
+    nav.classList.toggle('active')
+})
+
+function fecharNav() {
+    let nav = document.querySelector(".navMobile")
+    let input = document.querySelector("#menu-hamburguer")
+    input.checked = false;
+    nav.classList.toggle('active')
+}
+
+const fecharMobile = () => {
+    let nav = document.querySelector(".navMobile")
+    nav.classList.remove('active')
+    removerFundo()
+}
