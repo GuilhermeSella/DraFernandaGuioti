cdb540b4c245ae86cad76ac3c4d5de6ed078ae36
Guilherme Sella <guilhermesella1974@gmail.com>
2023-10-30 22:27:12 -0300

    feat: imagem banner

--- main.js
+++ main.js
@@ -1,8 +1,10 @@
 const header = document.querySelector('.header')
+const hamburguer = document.querySelector('.hamburguer')
 
 window.addEventListener("scroll", () => {
 
     header.classList.toggle('scroll', window.scrollY > 7)
+    hamburguer.classList.toggle('scroll', window.scrollY > 7)
 });
 
 const menuHambuguer = document.querySelector("#menu-hamburguer")
@@ -25,4 +27,24 @@ const fecharMobile = () => {
     let nav = document.querySelector(".navMobile")
     nav.classList.remove('active')
     removerFundo()
+}
+
+
+
+// ACCORDION
+
+
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    }
+  });
 }
