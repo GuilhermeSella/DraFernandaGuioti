1fd1860f68a5638ef865ff75efe8c9558e8af0e0
Guilherme Sella <guilhermesella1974@gmail.com>
2023-10-31 23:15:18 -0300

    feat: add section Doenças tratadas

--- main.js
+++ main.js
@@ -1,50 +1,32 @@
 const header = document.querySelector('.header')
-const hamburguer = document.querySelector('.hamburguer')
+// const hamburguer = document.querySelector('.hamburguer')
 
 window.addEventListener("scroll", () => {
 
     header.classList.toggle('scroll', window.scrollY > 7)
-    hamburguer.classList.toggle('scroll', window.scrollY > 7)
+    // hamburguer.classList.toggle('scroll', window.scrollY > 7)
 });
 
-const menuHambuguer = document.querySelector("#menu-hamburguer")
+// const menuHambuguer = document.querySelector("#menu-hamburguer")
 
-menuHambuguer.addEventListener("click", () => {
+// menuHambuguer.addEventListener("click", () => {
 
-    let nav = document.querySelector(".navMobile")
-    menuHambuguer.classList.toggle("hamburguerActive")
-    nav.classList.toggle('active')
-})
+//     let nav = document.querySelector(".navMobile")
+//     menuHambuguer.classList.toggle("hamburguerActive")
+//     nav.classList.toggle('active')
+// })
 
-function fecharNav() {
-    let nav = document.querySelector(".navMobile")
-    let input = document.querySelector("#menu-hamburguer")
-    input.checked = false;
-    nav.classList.toggle('active')
-}
+// function fecharNav() {
+//     let nav = document.querySelector(".navMobile")
+//     let input = document.querySelector("#menu-hamburguer")
+//     input.checked = false;
+//     nav.classList.toggle('active')
+// }
 
-const fecharMobile = () => {
-    let nav = document.querySelector(".navMobile")
-    nav.classList.remove('active')
-    removerFundo()
-}
-
-
-
-// ACCORDION
+// const fecharMobile = () => {
+//     let nav = document.querySelector(".navMobile")
+//     nav.classList.remove('active')
+//     removerFundo()
+// }
 
 
-var acc = document.getElementsByClassName("accordion");
-var i;
-
-for (i = 0; i < acc.length; i++) {
-  acc[i].addEventListener("click", function() {
-    this.classList.toggle("active");
-    var panel = this.nextElementSibling;
-    if (panel.style.maxHeight) {
-      panel.style.maxHeight = null;
-    } else {
-      panel.style.maxHeight = panel.scrollHeight + "px";
-    }
-  });
-}
